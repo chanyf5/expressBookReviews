29487648e764753540e8ab8a84ce78695af3933b
chanyf5 <chanyf@hotmail.com>
2024-04-30 23:00:02 -0400

    renew

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,7 +13,8 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  res.send(JSON.stringify(books));
+  let allBook = JSON.stringify(books);
+  res.send(JSON.stringify(allBook));
   
 });
 
@@ -21,27 +22,40 @@ public_users.get('/',function (req, res) {
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   let isbn = req.params.isbn;
-  let allBook = Object.values(books);
-  let isbnBook = allBook.find(book=>book.isbn === isbn);
+  let bookDetails = Object.values(books);
+  let isbnBook = bookDetails.find(b=>b.isbn === isbn);
   res.send(JSON.stringify(isbnBook));
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author = req.params.author;
+  bookDetails = Object.values(books);
+  let authorBook = bookDetails.find(b=>b.author===author);
+  res.send(JSON.stringify(authorBook));
+  
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let title = req.params.title;
+  let bookDetails = Object.values(books);
+  let titleBook = bookDetails.find(b=>b.title===title);
+  res.send(JSON.stringify(titleBook));
+  
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn;
+  let bookDetails = Object.values(books);
+  let reviewBook = bookDetails.find(b=>b.isbn===isbn);
+  const review = reviewBook.reviews;
+  res.send(review);
+  
 });
 
 module.exports.general = public_users;
