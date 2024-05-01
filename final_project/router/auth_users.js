6858d94357b4beeb62f09803cfb323c8174dbd78
chanyf5 <chanyf@hotmail.com>
2024-05-01 06:16:51 -0400

    again

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,7 +3,11 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [];
+let users = [
+    { "username": "user1",
+       "password": "pw1001"
+    }
+];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,37 +4,18 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
-const isValid = (username)=>{ //returns boolean
-    //write code to check is the username is valid
-        let userWithName = users.filter((user)=>{
-            return user.username === username
-        });
-        if(userWithName.length >0){
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-
 public_users.post("/register", (req,res) => {
   //Write your code here
-  const username = req.body.username;
-  const password = req.body.password;
-  
-  
-  
-  if (username && password) {
-    if (!isValid(username)) {
-        users.push({"username":username, "password":password});
-        return res.status(200).json({message: "User successfully registered."});
-    } else {
-        return res.status(404).json({message: "User already exists."});
-    }
+  const {username, password} = req.body;
+  if(!username || !password) {
+    return res.status(404).send({message: "User cannot be registered."});
   }
-  
-  return res.status(404).json({message: "Unable to register user."});
-  
+  const existUser =users.find(user=>user.username==username);
+  if(existUser) {
+    return res.status(403).send({message: "User already exist."});
+  }
+  users.push({username,password});
+  res.status(200).send({message: "User successfully registered"});
 });
 
 // Get the book list available in the shop
