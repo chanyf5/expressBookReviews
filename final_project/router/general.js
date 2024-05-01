fcfe8b819a3dec287d3c2c65b03a5854710ca618
chanyf5 <chanyf@hotmail.com>
2024-05-01 01:35:07 -0400

    gen.js done

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,7 +7,30 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  const alreadyExit = (username)=>{
+    let userWithName = users.filter((user)=>{
+        return user.username ===username
+    });
+    if(userWithName.length >0){
+        return true;
+    } else {
+        return false;
+    }
+  }
+  
+  
+  if (username && password0) {
+    if (!alreadyExit(username)) {
+        users.push({"username":username, "password":password});
+        return res.status(200).json({message: "User successfully registered."});
+    } else {
+        return res.status(404).json({message: "User already exists."});
+    }
+  }
+  return res.status(404).json({message: "Unable to register user."});
+  
 });
 
 // Get the book list available in the shop
