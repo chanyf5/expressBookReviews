539a622b65db9bfc3cd34297fab29b25142ce1a6
chanyf5 <chanyf@hotmail.com>
2024-05-04 05:42:49 -0400

    last

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,8 +21,8 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  let allBook = JSON.stringify(books);
-  res.send(JSON.stringify(allBook));
+  //let allBook = JSON.stringify(books);
+  res.send(JSON.stringify(books));
   
 });
 
